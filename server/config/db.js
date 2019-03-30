c6d3713d0c902c6e132eb4435b91cc0d168750b9
chandni <chandptl.91@gmail.com>
2019-03-30 12:45:57 -0400

    project and services added

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 //"URI" : "mongodb://localhost/test",
-"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
+//"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
+"URI": "mongodb://chandnipatel:a12345@ds153495.mlab.com:53495/comp308-w2019-assignment2",
 "secret": "SomeSecret"
 }
